166f1116218145aff44d9b411d9ff48f9f519dcc
Dincz <dineshndr02@gmail.com>
2023-04-19 20:28:41 +0530

    Build app.js

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+// require("./").config();
+
+
+const port = process.env.PORT  || 3000
+ 
+
+//Routes
+app.get('/hello', (req, res) =>{
+    res.send('HelloWorld')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
